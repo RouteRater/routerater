09baca216ea88e45532a94c56ee9a122fe1ddfe0
Stuart <gravatar@strudel.org.uk>
2015-04-22 22:35:00 +0100

    Make fullscreen mode for more info box

--- js/routerater.js
+++ js/routerater.js
@@ -121,7 +121,8 @@ RouteRater.prototype.makeMap = function(){
 	html += '<div id="gradedescriptiontoggle">\?</div>';
 	html += '<div id="gradedescription"></div>';
 	$('#grade').html(html).hide();
-	$(document).on('click','#gradedescriptiontoggle',function(e){ $('#gradedescription').toggle(); });
+	
+	var lb = new lightbox('#gradedescription','#gradedescriptiontoggle',{'max-width':'800px'});
 	$(document).on('click','.gradeselection li div',{me:this},function(e){ e.data.me.setRoad($(this).attr("class")); })
 
 	// Build typeahead events
@@ -734,6 +735,86 @@ function friendlyTime(d){
 		}
 	}
 	return "";
+}
+
+function lightbox(content,trigger,options){
+	if(!content || !trigger) return this;
+
+	this.content = content;
+	this.trigger = trigger;
+	this.options = (options) ? options : {}
+	if(!this.options.speed) this.options.speed = 500;
+	if(!this.options['max-width']) this.options['max-width'] = '500px';
+	this.lb = $(this.content);
+
+	this.style = {
+		'top':{ 'z-index': 1002, 'left':'0px','top':'0px','bottom':'0px','right':'0px','width':'100%','position': 'absolute', 'background-color': 'white' },
+		'inner':{'max-width':this.options['max-width'],'margin':'auto'}
+	}
+
+	// Trigger the lightbox
+	$(document).on('click',this.trigger,{me:this},function(e){
+		e.preventDefault();
+		// Event to open full screen mode
+		e.data.me.fullscreen();
+	});
+	
+	// Close the lightbox
+	$(document).on('click','.lightbox_top .close',{me:this},function(e){
+		e.preventDefault();
+		e.data.me.close();
+	});
+	
+	// Deal with the window changing size
+	var _obj = this;
+	$(window).resize(function(){ _obj.centre(); });
+	
+	// Hide the content
+	$(this.content).hide()
+
+	return this;
+}
+
+lightbox.prototype.centre = function(){
+	if($('.lightbox_top').is(':visible')){
+		$('.lightbox_top').css(this.style.top).css({'height':$(document).height()+'px'});
+
+		// Change top margin of lightbox_inner to keep it centred
+		var t = $(window).scrollTop();
+		$('.lightbox_top .close').css({'top':(t+10)+'px'});
+		if($(window).height() > this.lb.height()) t += (($(window).height()-this.lb.outerHeight())/2);
+		$('.lightbox_inner').css(this.style.inner).css({'margin-top':t+'px'});
+	}
+	return this;
+}
+
+lightbox.prototype.fullscreen = function(){
+
+	// Wrap out content
+	$(this.content).show().wrap("<div class='lightbox_top'><div class='lightbox_inner'><\/div><\/div>");
+
+	// Add close button
+	if(this.lb.find('.close').length==0) $('.lightbox_top').prepend('<a href="#" class="close">&times;<\/a>');
+	$('.lightbox_top .close').css({'font-size': '2em','line-height': '0.8em','position': 'absolute', 'right': '10px','text-decoration': 'none', 'top': '10px','color':'inherit'});
+		
+	// Centre it
+	this.centre();
+
+	// Remove scroll bars from page
+	$('body').css('overflow-y','hidden');
+
+	return this;
+}
+
+lightbox.prototype.close = function(){
+	if(this.lb.length > 0){
+		this.lb.attr('role','').fadeOut(this.options.speed,function(){
+			$('.lightbox_top .close').remove();
+			$(this).unwrap().unwrap();
+			$('body').css('overflow-y','auto');
+		});
+	}
+	return this;
 }
 
 $(document).ready(function(){
